9ff8168c349042d60f1fabab8dc89cf7b0c305e9
Nadim Taghiyev <nadimtaghiyev@Nadims-MacBook-Pro.local>
2025-07-06 21:46:27 -0400

    New projectwith Burak Kara

---
+++ src/pratik/Burak/PratikWithBurakFirstClass.java
@@ -0,0 +1,11 @@
+package pratik.Burak;
+
+public class PratikWithBurakFirstClass {
+	
+	
+	 public static void main(String[] args) {
+		   System.out.println("Pratik with  Burak new project first class");
+		   
+	}
+
+}
