3ee9fdce9345fb698a8ed4fb915ab54c500fc0b8
burak <burakkara0090@gmail.com>
2025-07-06 22:18:44 -0500

    second uodate

--- src/pratik/Burak/BranchPull1.java
+++ src/pratik/Burak/BranchPull1.java
@@ -4,7 +4,11 @@ public class BranchPull1 {
 	
 	
 	public static void main(String[] args) {
-		System.out.println("Ben burada deyisiklik yapdim. Branch merge online and   pull... burak kara");
+
+		System.out.println("Ben burada deyisiklik yapdim. Branch merge online and   pull... burak kara.. ben tekrar buradayim");
+
+		System.out.println("burada degiliskler yaptim. Branch merge online and   pull... burak kara.. bende buradaki degisilikler yapacagim");
+
 		
 		
 		
