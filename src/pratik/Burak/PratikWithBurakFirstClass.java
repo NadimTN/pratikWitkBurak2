9e8358b0fc45fcdcb48fb738d0046d957e97eb73
burak <burakkara0090@gmail.com>
2025-07-06 20:55:08 -0500

    first commit

--- src/pratik/Burak/PratikWithBurakFirstClass.java
+++ src/pratik/Burak/PratikWithBurakFirstClass.java
@@ -2,7 +2,7 @@ package pratik.Burak;
 
 public class PratikWithBurakFirstClass {
 	
-	
+	// burak kara
 	 public static void main(String[] args) {
 		   System.out.println("Pratik with  Burak new project first class");
 		   
