f89ec84e102887ece556a12baec66cb934c7c735
Nadim Taghiyev <nadimtaghiyev@Nadims-MacBook-Pro.local>
2025-07-06 23:30:15 -0400

    Conflic creation for practi

--- src/pratik/Burak/PratikWithBurakFirstClass.java
+++ src/pratik/Burak/PratikWithBurakFirstClass.java
@@ -3,8 +3,11 @@ package pratik.Burak;
 public class PratikWithBurakFirstClass {
 	
 	// burak kara
+	
+	//  ayni seyleri ekliyorumki conflict olussun 
+	// bende buraya bir seyler ekliyorum 
 	 public static void main(String[] args) {
-		   System.out.println("Pratik with  Burak new project first class");
+		   System.out.println("Pratik  bende buraya calismaya basladim with  Burak new project first class");
 		   
 	}
 
