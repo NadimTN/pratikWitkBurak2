dfa893804aeb558e5f3700b156c44a9eec1d842f
Nadim Taghiyev <nadimtaghiyev@Nadims-MacBook-Pro.local>
2025-07-06 23:08:09 -0400

    Conflict creation

--- src/pratik/Burak/BranchPratikWithBurak.java
+++ src/pratik/Burak/BranchPratikWithBurak.java
@@ -3,7 +3,7 @@ package pratik.Burak;
 public class BranchPratikWithBurak {
 	
 	public static void main(String[] args) {
-		System.out.println("Branch pratik with Burak");
+		System.out.println("Branch conflict and solution Branch pratik with Burak");
 	}
 
 }
