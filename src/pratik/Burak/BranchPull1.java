9f8a2cb8c243f2221b22421e1ccbf4d9f1c3eee6
Nadim Taghiyev <nadimtaghiyev@Nadims-MacBook-Pro.local>
2025-07-06 23:10:46 -0400

    BranchPull1 classda deysiklik yapdim konflikt icin

--- src/pratik/Burak/BranchPull1.java
+++ src/pratik/Burak/BranchPull1.java
@@ -4,7 +4,7 @@ public class BranchPull1 {
 	
 	
 	public static void main(String[] args) {
-		System.out.println("Branch merge online and   pull... burak kara");
+		System.out.println("Ben burada deyisiklik yapdim. Branch merge online and   pull... burak kara");
 		
 		
 		
