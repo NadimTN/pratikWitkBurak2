23ee55b654997980768d07b05e5c61754426ff57
burak <burakkara0090@gmail.com>
2025-07-06 22:03:49 -0500

    new update

--- src/pratik/Burak/BranchPull1.java
+++ src/pratik/Burak/BranchPull1.java
@@ -4,7 +4,12 @@ public class BranchPull1 {
 	
 	
 	public static void main(String[] args) {
-		System.out.println("Branch merge online and   pull ");
+		System.out.println("Branch merge online and   pull... burak kara");
+		
+		
+		
+	
 	}
 
+	
 }
