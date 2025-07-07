6f3f484acee44213e81b7f303ecd57b248ad5321
Nadim Taghiyev <nadimtaghiyev@Nadims-MacBook-Pro.local>
2025-07-06 22:19:17 -0400

    Branch pratik burak1

--- src/pratik/Burak/PratikWithBurakFirstClass.java
+++ src/pratik/Burak/PratikWithBurakFirstClass.java
@@ -6,6 +6,16 @@ public class PratikWithBurakFirstClass {
 	 public static void main(String[] args) {
 		   System.out.println("Pratik with  Burak new project first class");
 		   
+		   
+		   
+		   
+		   System.out.println("Ben burada calisiyorum");
+		   
+		   System.out.println("Ben kendi isimi bitirdim. Simdi ben gonderiyorum online repository");
+		  
+		   
+		   
+		   
 	}
 
 }
